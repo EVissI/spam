dfe2bf92cf7f71f9691f71d7758aa5e563213968
viss <egor-berezin@bk.ru>
2025-06-28 09:22:53 +1000

    commit

--- app/aiogram/routers/init_router.py
+++ app/aiogram/routers/init_router.py
@@ -5,7 +5,8 @@ from app.aiogram.routers.add_preset_router import add_preset_router
 from app.aiogram.routers.add_acount_router import add_account_router
 from app.aiogram.routers.start_router import start_router
 from app.aiogram.routers.my_accounts import my_accounts_router
+from app.aiogram.routers.add_account_tdata import add_tdata_router
 
 init_router = Router()
 init_router.include_routers(start_router,my_accounts_router, 
-                            add_account_router, add_preset_router)
+                            add_account_router, add_preset_router,add_tdata_router)
